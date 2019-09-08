1d00bb2af1a1a1ca2e0fdf59e4ed8187c5d9247f
Venky Venkataraman <venky76v@gmail.com>
2019-09-08 23:42:00 +0100

    Quick test to see if I can integrate with the release pipeline

--- WebApp/ArticleRepository.cs
+++ WebApp/ArticleRepository.cs
@@ -47,7 +47,7 @@ namespace WebApp
                 Title = "What is Docker",
                 Author= "Venky Venkataraman",
                 PublishedOn = DateTime.Now,
-                Content = "It works on my machine but it doesn't work on the production server"
+                Content = "It works on my machine but it doesn't work on the production server. Docker is a software platform for building applications based on containers — small and lightweight execution environments that make shared use of the operating system kernel but otherwise run in isolation from one another. While containers as a concept have been around for some time, Docker, an open source project launched in 2013, helped popularize the technology, and has helped drive the trend towards containerization and microservices in software development that has come to be known as cloud-native development"
             },
         };
 
